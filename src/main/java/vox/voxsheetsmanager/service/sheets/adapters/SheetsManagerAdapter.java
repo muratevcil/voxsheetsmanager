17798bffe73860c93db8197b11e6e1e0cbc6be35
Murat Evcil <muratevcilf@gmail.com>
2025-01-30 16:01:29 +0300

    Commit

--- src/main/java/vox/voxsheetsmanager/service/sheets/adapters/SheetsManagerAdapter.java
+++ src/main/java/vox/voxsheetsmanager/service/sheets/adapters/SheetsManagerAdapter.java
@@ -1,9 +1,15 @@
 package vox.voxsheetsmanager.service.sheets.adapters;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import vox.voxsheetsmanager.data.sheets.requests.GetSheetRequest;
 import vox.voxsheetsmanager.service.sheets.ports.SheetsManagerPort;
 
 public class SheetsManagerAdapter implements SheetsManagerPort {
+    @Override
+    public ObjectNode getSheet(GetSheetRequest getSheetRequest) {
+        return null;
+    }
+
     @Override
     public ObjectNode getSheet(String spreadName, String sheetName) {
         return null;
--- src/main/java/vox/voxsheetsmanager/service/sheets/ports/SheetsManagerPort.java
+++ src/main/java/vox/voxsheetsmanager/service/sheets/ports/SheetsManagerPort.java
@@ -1,10 +1,14 @@
 package vox.voxsheetsmanager.service.sheets.ports;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import vox.voxsheetsmanager.data.sheets.requests.GetSheetRequest;
 
 public interface SheetsManagerPort {
 
     ObjectNode getSheet(GetSheetRequest getSheetRequest);
+
+    ObjectNode getSheet(String spreadName, String sheetName);
+
     ObjectNode updateSheet(String spreadName, String sheetName, ObjectNode sheetContent);
     ObjectNode deleteSheet(String spreadName, String sheetName);
 
